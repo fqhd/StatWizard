6be0489975ad3a1a6b6e03c93fdb1e2e8c51fb1b
Froglodyte <69530598+Froglodyte@users.noreply.github.com>
2022-03-12 12:59:55 +0530

    Uploads command module for info command

---
+++ command modules/infocmd.js
@@ -0,0 +1,61 @@
+import DiscordJS, { MessageEmbed } from 'discord.js'
+
+export default function serverInfo(message){
+
+    var server = message.guild;
+
+    const  infoEmbed = new MessageEmbed()
+    .setColor('#b6f542')
+    .setTitle(`**Info about ${server.name}**`)
+    .setAuthor(server.name, server.iconURL({dynamic: true}))
+    .addFields(
+        {
+            name: "📚 | GENERAL INFO:",
+            value:
+            `
+
+‣ Name: ${server.name}
+‣ Owner: <@${server.ownerId}>
+
+‣ Description: ${server.description}
+            `
+        },
+        {
+            name: "👥 | USERS:",
+            value:
+            `
+‣ Members: ${server.members.cache.filter((m) => !m.user.bot).size}
+‣ Bots: ${server.members.cache.filter((m) => m.user.bot).size}
+
+• Total: ${server.members.cache.size}
+            `
+        },
+        {
+            name: "📋 | CHANNELS:",
+            value:
+            `
+‣ Text: ${server.channels.cache.filter((c) => c.type === 'GUILD_TEXT').size}
+‣ Voice: ${server.channels.cache.filter((c) => c.type === 'GUILD_VOICE').size}
+‣ Threads: ${server.channels.cache.filter((c) => c.type === 'GUILD_NEW_THREAD' && 'GUILD_PRIVATE_THREAD' && 'GUILD_PUBLIC_THREAD').size}
+‣ Categories: ${server.channels.cache.filter((c) => c.type === 'GUILD_CATEGORY').size}
+‣ Stages: ${server.channels.cache.filter((c) => c.type === 'GUILD_STAGE_VOICE').size}
+
+• Total: ${server.channels.cache.size}
+            `
+        },
+        {
+            name: "🎭 | EMOJIS & STICKERS:",
+            value:
+            `
+‣ Static: ${server.emojis.cache.filter((e) => !e.animated).size}
+‣ Animated: ${server.emojis.cache.filter((e) => e.animated).size}
+‣ Stickers: ${server.stickers.cache.size}
+
+• Total: ${server.emojis.cache.size + server.stickers.cache.size}
+            `
+        }
+
+    )
+    .setFooter(`last updated on: ${new Date().toLocaleString()}`)
+    message.reply({ embeds: [infoEmbed] });
+}
