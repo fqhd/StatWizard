6530c57f8e492597f379022fcbd5efdff240127f
Froglodyte <69530598+Froglodyte@users.noreply.github.com>
2022-03-14 14:16:16 +0530

    added ping command

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import DiscordJS, { Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 import { command_map } from './command_modules/command_handler.js';
+import serverInfo from './command modules/infocmd.js'
+import ping from './command modules/pingcmd.js'
 
 const client = new DiscordJS.Client({
     intents:[
@@ -18,10 +20,21 @@ client.on('messageCreate', (message) => {
     if(!message.content.startsWith('-') || message.author.bot) return;
     const tokens = message.content.split(' ');
     const command = tokens[0].slice(1);
-    const args = tokens.slice(1);
+    const args = tokens.slice(1).join(' ');
     if(command_map[command]){
         command_map[command](client, message, args);
     }
+    
+    switch(command){
+        case 'info':
+            message.reply({ embeds: [serverInfo(message)] });
+            break;
+
+        case 'ping':
+            message.reply({ embeds: [ping(message, client)] });
+            break;
+    }
+    
 });
 
 
